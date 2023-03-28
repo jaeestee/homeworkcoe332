b6078b6ad641c4b361f956131b30fce794d8af89
jaeestee <oh.jae.steven@gmail.com>
2023-03-28 07:05:57 +0000

    added the rest of the queries needed to the gene_api script

--- homework06/gene_api.py
+++ homework06/gene_api.py
@@ -60,7 +60,7 @@ def data() -> dict:
     return DATA
 
 @app.route('/genes', methods=['GET'])
-def epoch_data() -> list:
+def gene_ids() -> list:
     """
     This function calls the get_data() function to retrieve the entire data set and returns the listOfEpochs
     variable. It can take in query parameters of offset and limit which will cause the data to start at a
@@ -74,7 +74,7 @@ def epoch_data() -> list:
     #try-except block that makes sure it returns a message if the data is empty or doesn't exist
     try:
         #stores the entire epoch data by navigating through the entire data dictionary
-        listOfGenes = data()['responses']['docs']['hgnc_id']
+        listOfGenes = data()['response']['docs']
     except TypeError:
         return 'The data set does not exist yet!\n'
     except KeyError:
@@ -84,13 +84,13 @@ def epoch_data() -> list:
     results = []
 
     #for loop that stores the requested Epoch data
-    for i in range(limit):
-        results.append(listOfEpochs[i])
+    for i in range(len(listOfGenes)):
+        results.append(listOfGenes[i]['hgnc_id'])
     
     return results
 
-@app.route('/epochs/<string:epoch>', methods=['GET'])
-def specific_epoch_data(epoch: str) -> dict:
+@app.route('/genes/<string:geneID>', methods=['GET'])
+def specific_gene_data(geneID: str) -> dict:
     """
     This function returns the specific epoch data that was requested by the user.
 
@@ -100,89 +100,24 @@ def specific_epoch_data(epoch: str) -> dict:
     Returns:
         epochData (dict): The epoch data for the given epoch key.
     """
-
-    #stores the list of epochs using the pre-existing function
-    listOfEpochs = epoch_data()
-
+    
     #try-except block to make sure the data has information
     try:
-        listOfEpochs[0]['EPOCH']
+        listOfGenes = data()['response']['docs']
+    except TypeError:
+        return 'The data seems to be empty or does not exist...\n'
+    except NameError:
+        return 'The data seems to be empty or does not exist...\n'
     except TypeError:
         return 'The data seems to be empty or does not exist...\n'
 
     #shorts through the list to match the epoch key and returns the data for it
-    for i in range(len(listOfEpochs)):
-        if listOfEpochs[i]['EPOCH'] == epoch:
-            return listOfEpochs[i]
+    for i in range(len(listOfGenes)):
+        if listOfGenes[i]['hgnc_id'] == geneID:
+            return listOfGenes[i]
 
     #if it doesn't find it, returns this prompt
     return 'Could not find the epoch for the given key.\n'
-
-@app.route('/epochs/<string:epoch>/speed', methods=['GET'])
-def calculate_epoch_speed(epoch: str) -> dict:
-    """
-    This function calculates the speed for the specific epoch and returns it.
-
-    Args:
-        epoch (str): The specific epoch key to find the requested epoch data.
-
-    Returns:
-        speed (float): The speed for the specific epoch requested.
-    """
-
-    #stores the specific epoch using the pre-existing function
-    specificEpoch = specific_epoch_data(epoch)
-
-    #try-except block to make sure the data has information
-    try:
-        specificEpoch['X_DOT']['#text']
-    except TypeError:
-        return 'Could not calculate the speed of the epoch for the given key.\n'
-        
-    #stores the X_DOT, Y_DOT, and Z_DOT for the specific epoch into corresponding variables and converts them to float
-    xDot = float(specificEpoch['X_DOT']['#text'])
-    yDot = float(specificEpoch['Y_DOT']['#text'])
-    zDot = float(specificEpoch['Z_DOT']['#text'])
-
-    #the units for the vector
-    units = specificEpoch['X_DOT']['@units']
-    
-    #calculates the speed using the magnitude of a vector formula
-    speed = math.sqrt(xDot**2 + yDot**2 + zDot**2)
-
-    return f'Speed: {str(speed)} {units}\n'
-
-@app.route('/help', methods=['GET'])
-def help() -> str:
-    """
-    This function returns a human readable string that explains all the available
-    routes in this API.
-
-    Returns:
-       helpOutput (str): The string that explains the routes.
-    """
-
-    helpOutput = '''usage: curl localhost:5000[<route>][?<query parameter>]\n
-The different possible routes:
-    /                                   Returns the entire data set
-    /epochs                             Returns the list of all Epochs in the data set
-    /epochs/<epoch>                     Returns the state vectors for a specific Epoch from the data set
-    /epochs/<epoch>/speed               Returns the instantaneous speed for a specific Epoch in the data set
-    /help                               Returns the help text taht describes each route
-    /delete-data                        Deletes all the data from the dictionary
-    /post-data                          Reloads the dictionary with data from the website
-
-The different query parameters (only works for the "/epochs" route):
-    limit=<int>                         Returns a specific integer amount of Epochs from the data set
-    offset=<int>                        Returns the entire data set starting offset by a certain integer amount
-    limit=<int>'&'offset=<int>          Combining the limit and offset query parameters
-
-    example:
-    /epochs?limit=15'&'offset=3         Returns the 15 Epochs from the data set offset by 3
-
-'''
-    
-    return helpOutput
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
